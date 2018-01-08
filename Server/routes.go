f8410f0ac27840cad133636cb767d2da0ed640c2
Adria Ros Saez <adria.ros.saez@everis.com>
2018-01-08 12:48:02 +0100

    Add query function in order to send the specific dialogFlow query

--- Server/routes.go
+++ Server/routes.go
@@ -3,12 +3,26 @@ package main
 import (
 	"github.com/labstack/echo"
 	"net/http"
+	"github.com/labstack/gommon/log"
 )
 
 // Root is the root route to test service health
 func Root(c echo.Context) error {
 
-	initDialogFlow()
+	client, err := initDialogFlow()
+
+	if err != nil {
+		log.Print("Error initDialogFlow", err)
+	}
+
+	resp, err := dialogFlowQuery(client, "Hello")
+
+	if err != nil {
+		log.Print("Server Error: ", resp, " DialogFlow Error: ", err)
+		return c.NoContent(http.StatusExpectationFailed)
+	}
+
+	println("Respuesta: ", resp)
 
 	return c.NoContent(http.StatusOK)
 }
